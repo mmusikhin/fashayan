33cae981935fc9bb5e641a577cde4e68371e871c
MikhailMusikhin <mikhail.99925@gmail.com>
2026-05-14 23:29:51 +0300

    mobile optimisation #11

--- js/exhibition.js
+++ js/exhibition.js
@@ -35,7 +35,7 @@ function setViewUI(isOn) {
 
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setPixelRatio(
-  isMobile ? 2 : Math.min(window.devicePixelRatio || 1, 2),
+  isMobile ? 1.5 : Math.min(window.devicePixelRatio || 1, 2),
 );
 renderer.setSize(container.clientWidth, container.clientHeight);
 renderer.outputColorSpace = THREE.SRGBColorSpace;
